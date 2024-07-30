2324654dcf65c0b5c3bb528f2d8ad319f3c3d189
Adithyan78 <160298389+Adithyan78@users.noreply.github.com>
2024-07-30 06:27:23 +0000

    final commit

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material'
 const Books = () => {
   return (
     <div>
-      <Typography>this is the book page 2</Typography>
+      <Typography>this is the book page 3</Typography>
     </div>
   )
 }
